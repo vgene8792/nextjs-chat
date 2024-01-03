fc6040a0694ce6f39413b510a595d4ca10a9df29
vgene8792 <155517239+vgene8792@users.noreply.github.com>
2024-01-03 15:00:50 +0800

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -44,7 +44,7 @@ async function UserOrLogin() {
           <UserMenu user={session.user} />
         ) : (
           <Button variant="link" asChild className="-ml-2">
-            <Link href="/sign-in?callbackUrl=/">Login</Link>
+            <Link href="/sign-in?callbackUrl=/">LOGIN</Link>
           </Button>
         )}
       </div>
