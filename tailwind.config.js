f168d73d60dbbae739075fca1e82cd6f57985665
vgene8792 <155517239+vgene8792@users.noreply.github.com>
2024-01-03 17:30:37 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],*/
+  darkMode: ['class'],
   content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
   theme: {
     container: {
