9f1926e4b43e577d6dc3cf7e4ecfadef1488881b
vgene8792 <155517239+vgene8792@users.noreply.github.com>
2024-01-04 11:42:16 +0800

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -26,13 +26,8 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
         <h1 className="mb-2 text-lg font-semibold">
           Welcome to GenLite Genie AI Chatbot!
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+        <p className="mb-2 text-lg font-semibold">
+          How can i assist you today
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
